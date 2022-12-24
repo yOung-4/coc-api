8da43c48faa161955b84873dd9bef5b910844e5d
yOung-4 <91882244+yOung-4@users.noreply.github.com>
2022-12-24 15:58:28 +0800

    添加-注册api

--- api/sign_up/__init__.py
+++ api/sign_up/__init__.py
@@ -1,23 +1,36 @@
 # fastapi相关导入
-from fastapi import APIRouter, Form, HTTPException
+from fastapi import APIRouter, Form
 # sql相关导入
 from sqlalchemy import select
 from database import session
+from database import engine
 from database import User
 # 杂项导入
-from verify_email import verify_email
-from database import Base
+from email_validator import validate_email, EmailNotValidError
+from passlib.context import CryptContext
 
 router = APIRouter()
+session = session()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 # 处理提交的新用户注册表单
 @router.put("/sign/")
 def sign_up(email: str = Form(), password: str = Form(), nick_name: str = Form()):
-    if verify_email(email) is False:
-        raise HTTPException(status_code=400, detail='email address is wrong')
-    if not session.execute(select(User).where(User.email == email)):
-        raise HTTPException(status_code=400, detail='email already exits')
-    new_user = User(email = email, password = password, nick_name = nick_name)
-    session.add(new_user)
-    session.commit()
+    # 校验email地址是否合法并将其标准化
+    try:
+        email = validate_email(email, check_deliverability=True).email
+    except EmailNotValidError:
+        return {'status code': 'wrong email'}
+    # 检查用户是否已经注册
+    stmt = select(User).where(User.email == email)
+    result = session.execute(stmt)
+    result = result.scalar()
+    if result is not None:
+        return {'status code': 'exited email'}
+    else:
+        # 将新用户的数据写入数据库，注意密码是经过hash的
+        new_user = User(email=email, password=pwd_context.hash(password), nick_name=nick_name)
+        session.add(new_user)
+        session.commit()
+        return {'status code': 'sign up OK'}
--- database/__init__.py
+++ database/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine("postgresql://web:kokodaoyo@127.0.0.1:5432/api", future=True, echo=True)
+engine = create_engine("postgresql://web:kokodayo@127.0.0.1:5432/api", future=True, echo=False)
 session = sessionmaker(engine)
 
 Base = declarative_base()
@@ -14,3 +14,5 @@ class User(Base):
     email = Column(String)
     nick_name = Column(String)
     password = Column(String)
+
+# User.__table__.create(engine)
